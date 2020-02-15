21b90f8acfe39740d71d000e2df12aeaf1f56ffe
ch3njust1n <ch3njus@gmail.com>
2020-02-15 16:53:19 -0500

    Added argparse update support

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Move to /Users/justin/anaconda3/lib/python3.6/site-packages/ so can import from anywhere
+### Record
+Module for recording experiments
+
+### Examples
+
+
+### TODO
+1. Move to /Users/justin/anaconda3/lib/python3.6/site-packages/ so can import from anywhere
+
+2. CSV output support
+
+3. Reload
--- record.py
+++ record.py
@@ -16,7 +16,8 @@ class Record(object):
 			port (int)		MongoDB port number
 	'''
 	def __init__(self, host='localhost', port=27017, database='experiments', collection='parameters'):
-		os.mkdir('logs')
+		if not os.path.exists('logs'):
+			os.mkdir('logs')
 
 		# Setup logger
 		self.log = logging.getLogger()
@@ -55,9 +56,42 @@ class Record(object):
 	'''
 	def add(self, key, value):
 		if type(key) is not str:
-			raise Exception('Record.add() key parameter must be type String.')
+			raise Exception('Key parameter must be type String.')
 
+		if self.is_argparse(value):
+			value = vars(value)
+		
 		self.record[key] = value
+
+
+	'''
+	Update record with dictionary
+
+	inputs: obj (dict or argparse.Namespace)	Dictionary or argparse.Namespace with values to update record
+	'''
+	def extend(self, obj):
+		is_argparse = self.is_argparse(obj)
+
+		if type(obj) is not dict and not is_argparse:
+			raise Exception('Input must be a dictionary or argparse.Namespace')
+
+		if is_argparse:
+			obj = vars(obj)
+
+		self.record.update(obj)
+
+
+	'''
+	Check if argument is argparse.Namespace
+
+	inputs:  args (*) 	 	Variable
+	outputs: res  (bool)	True if args is an instance of argparse.Namespace, else False
+	'''
+	def is_argparse(self, args):
+		try:
+			return args.__module__ == 'argparse'
+		except AttributeError:
+			return False
 
 
 	'''
